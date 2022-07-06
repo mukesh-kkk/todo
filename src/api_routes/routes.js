58ac0fe3a3d259124412fcad19f25e7b59d0c85a
mukesh-kkk <mukesh.kushawha@saarthi.ai>
2022-07-06 12:26:56 +0530

    routes added

---
+++ src/api_routes/routes.js
@@ -0,0 +1,6 @@
+export const GET_TASKS="https://api-nodejs-todolist.herokuapp.com/task/"  //get
+export const POST_TASK="https://api-nodejs-todolist.herokuapp.com/task" //post
+export const DEL_TASK="https://api-nodejs-todolist.herokuapp.com/task/" //delete method
+export const UPDATE_TASK="https://api-nodejs-todolist.herokuapp.com/task/" //put
+export const REG_USER="https://api-nodejs-todolist.herokuapp.com/user/register" //post
+export const LOG_USER="https://api-nodejs-todolist.herokuapp.com/user/login" //post 
